dc044cea68db09220f2b7292da1cd6bc6b80f0d9
Bartosz Sosnowski <sosen21@gmail.com>
2023-09-03 02:30:40 +0200

    Added sdkman for Java. Maven and Gradle installation. TIBCO BWCE version upgraded to 2.8.3. Command replaces with other ansible modules where possible.

--- ansible_run.sh
+++ ansible_run.sh
@@ -1,1 +1,2 @@
-ansible-playbook -i ./inventories/production/hosts bwce_workstation.yml -vvv
+# ansible-playbook -i ./inventories/production/hosts bwce_workstation.yml -vvv
+ansible-playbook -i ./inventories/production/hosts bwce_workstation.yml
